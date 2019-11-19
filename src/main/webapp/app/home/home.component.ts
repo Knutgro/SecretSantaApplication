212fb2985b2ac6851bd91308e3a30ece6725e354
knutgro <kgrstad@gmail.com>
2019-11-19 21:36:01 +0100

    home page reworked to login, redirects changed

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ElementRef, Renderer } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
@@ -6,21 +6,37 @@ import { JhiEventManager } from 'ng-jhipster';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+import { LoginService } from 'app/core/login/login.service';
+import { StateStorageService } from 'app/core/auth/state-storage.service';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['home.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
+  authenticationError: boolean;
   account: Account;
   authSubscription: Subscription;
   modalRef: NgbModalRef;
+  loginForm = this.fb.group({
+    username: [''],
+    password: [''],
+    rememberMe: [false]
+  });
 
   constructor(
     private accountService: AccountService,
     private loginModalService: LoginModalService,
-    private eventManager: JhiEventManager
+    private eventManager: JhiEventManager,
+    private loginService: LoginService,
+    private stateStorageService: StateStorageService,
+    private elementRef: ElementRef,
+    private renderer: Renderer,
+    private router: Router,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -30,13 +46,14 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.registerAuthenticationSuccess();
   }
 
+  ngAfterViewInit() {
+    setTimeout(() => this.renderer.invokeElementMethod(this.elementRef.nativeElement.querySelector('#username'), 'focus', []), 0);
+  }
+
   registerAuthenticationSuccess() {
     this.authSubscription = this.eventManager.subscribe('authenticationSuccess', () => {
       this.accountService.identity().subscribe(account => {
         this.account = account;
-        if (this.account.authorities.length === 0) {
-          this.account = undefined;
-        }
       });
     });
   }
@@ -45,8 +62,55 @@ export class HomeComponent implements OnInit, OnDestroy {
     return this.accountService.isAuthenticated();
   }
 
-  login() {
+  login2() {
     this.modalRef = this.loginModalService.open();
+  }
+
+  login() {
+    this.loginService
+      .login({
+        username: this.loginForm.get('username').value,
+        password: this.loginForm.get('password').value,
+        rememberMe: this.loginForm.get('rememberMe').value
+      })
+      .subscribe(
+        () => {
+          this.authenticationError = false;
+          if (
+            this.router.url === '/account/register' ||
+            this.router.url.startsWith('/account/activate') ||
+            this.router.url.startsWith('/account/reset/')
+          ) {
+            this.router.navigate(['event']);
+          }
+
+          this.eventManager.broadcast({
+            name: 'authenticationSuccess',
+            content: 'Sending Authentication Success'
+          });
+
+          // previousState was set in the authExpiredInterceptor before being redirected to login modal.
+          // since login is successful, go to stored previousState and clear previousState
+
+          this.router.navigateByUrl('/event');
+          /*
+          const redirect = this.stateStorageService.getUrl();
+          if (redirect) {
+            this.stateStorageService.storeUrl(null);
+            this.router.navigateByUrl(redirect);
+          }
+          */
+        },
+        () => (this.authenticationError = true)
+      );
+  }
+
+  register() {
+    this.router.navigate(['/account/register']);
+  }
+
+  requestResetPassword() {
+    this.router.navigate(['/account/reset', 'request']);
   }
 
   ngOnDestroy() {
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { SecretSantaAppSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [SecretSantaAppSharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [SecretSantaAppSharedModule, RouterModule.forChild([HOME_ROUTE]), MatCardModule],
   declarations: [HomeComponent]
 })
 export class SecretSantaAppHomeModule {}
